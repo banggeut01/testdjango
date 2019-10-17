9e2f372ee3a2af770c9381e468388ef02c7304e1
banggeut01 <genie121110@gmail.com>
2019-10-17 19:58:27 +0900

    실습

---
+++ articles/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    # post요청
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article = article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/form.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
